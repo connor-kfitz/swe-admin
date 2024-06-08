dc0c837abf7c6556ece7d01f8492e7c5e049f989
connor-kfitz <connorkfitzsimmons@gmail.com>
2024-06-08 17:55:47 -0400

    Updating ENV Names

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -6,13 +6,13 @@ import { getStorage } from "firebase/storage";
 const developEnvironment = process.env.NODE_ENV === "development";
 
 const firebaseConfig = {
-  apiKey: developEnvironment ? process.env.REACT_APP_developFirebaseApiKey : process.env.REACT_APP_productionFirebaseAPIKey,
-  authDomain: developEnvironment ? process.env.REACT_APP_developFirebaseAuthDomain : process.env.REACT_APP_productionFirebaseAuthDomain,
-  projectId: developEnvironment ? process.env.REACT_APP_developFirebaseProjectId : process.env.REACT_APP_productionFirebaseProjectId,
-  storageBucket: developEnvironment ? process.env.REACT_APP_developFirebaseStorageBucket : process.env.REACT_APP_productionFirebaseStorageBucket,
-  messagingSenderId: developEnvironment ? process.env.REACT_APP_developFirebaseMessagingSenderId : process.env.REACT_APP_productionFirebaseMessagingSenderId,
-  appId: developEnvironment ? process.env.REACT_APP_developFirebaseAppId : process.env.REACT_APP_productionFirebaseAppId,
-  measurementId: developEnvironment ? process.env.REACT_APP_developFirebaseMeasurementIdb : process.env.REACT_APP_productionFirebaseMeasurementId
+  apiKey: developEnvironment ? process.env.REACT_APP_DEVELOP_FB_API_KEY : process.env.REACT_APP_PRODUCTION_FB_API_KEY,
+  authDomain: developEnvironment ? process.env.REACT_APP_DEVELOP_FB_AUTH_DOMAIN : process.env.REACT_APP_PRODUCTION_FB_AUTH_DOMAIN,
+  projectId: developEnvironment ? process.env.REACT_APP_DEVELOP_FB_PROJECT_ID : process.env.REACT_APP_PRODUCTION_FB_PROJECT_ID,
+  storageBucket: developEnvironment ? process.env.REACT_APP_DEVELOP_FB_STORAGE_BUCKET : process.env.REACT_APP_PRODUCTION_FB_STORAGE_BUCKET,
+  messagingSenderId: developEnvironment ? process.env.REACT_APP_DEVELOP_FB_MS_ID : process.env.REACT_APP_PRODUCTION_FB_MS_ID,
+  appId: developEnvironment ? process.env.REACT_APP_DEVELOP_FB_APP_ID : process.env.REACT_APP_PRODUCTION_FB_APP_ID,
+  measurementId: developEnvironment ? process.env.REACT_APP_DEVELOP_FB_MEASUREMENT_ID : process.env.REACT_APP_PRODUCTION_FB_MEASUREMENT_ID
 }
 
 const app = initializeApp(firebaseConfig);
