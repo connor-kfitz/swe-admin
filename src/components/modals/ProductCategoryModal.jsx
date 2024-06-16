c3828de35714b27b7fc2c53d2be9a7a3dc41eb00
connor-kfitz <connorkfitzsimmons@gmail.com>
2024-06-15 21:29:16 -0400

    Removed Image From Product Category

--- src/components/modals/ProductCategoryModal.jsx
+++ src/components/modals/ProductCategoryModal.jsx
@@ -1,59 +1,28 @@
-import { useState, useEffect, useRef } from "react";
-import { db, storage } from "../../firebase/firebase";
+import { useState, useEffect } from "react";
+import { db } from "../../firebase/firebase";
 import { collection, addDoc, doc, setDoc, Timestamp, serverTimestamp, query, where, getDocs, updateDoc } from "firebase/firestore";
-import { ref, uploadBytes, getDownloadURL, deleteObject } from "firebase/storage";
 
 class ProductCategory {
-  constructor(name, image) {
+  constructor(name) {
     this.name = name;
-    this.image = image;
   }
 }
 export default function ProductCategoryModal({ editProductCategoryData, setEditProductCategoryData, setProductCategories }) {
 
-  const [postData, setPostData] = useState(new ProductCategory("", "", "", "", { file: null, filePath: "", path: "", src: "" }, [], ""));
+  const [postData, setPostData] = useState(new ProductCategory(""));
   const [formErrors, setFormErrors] = useState({ name: false });
   const [loading, setLoading] = useState(false);
 
-  const deleteImageRef = useRef("");
-
   useEffect(() => {
     document.getElementById('product-category-modal').addEventListener("close", () => {
-      setPostData(new ProductCategory("", { file: null, filePath: "", path: "", src: "" }));
+      setPostData(new ProductCategory(""));
       setEditProductCategoryData(null);
-      deleteImageRef.current = "";
     });
   });;
 
   useEffect(() => {
-    if (editProductCategoryData) setPostData({ ...editProductCategoryData, "image": { file: null, filePath: "", path: editProductCategoryData.image.path, src: editProductCategoryData.image.src }});
+    if (editProductCategoryData) setPostData({ ...editProductCategoryData });
   }, [editProductCategoryData]);
-
-
-  async function uploadFiles() {
-    if (!postData.image.file) return;
-    const imageRef = ref(storage, `images/productCategories/${postData.image.file.name}`);
-    const upload = await uploadBytes(imageRef, postData.image.file);
-    const url = await getDownloadURL(upload.ref);
-    return { src: url, path: `images/productCategories/${postData.image.file.name}` };
-  };
-
-  async function deleteFile(path) {
-    const imageDocRef = ref(storage, path);
-    await deleteObject(imageDocRef);
-  }
-
-  function setImage(event) {
-    event.preventDefault();
-    deleteImageRef.current = postData.image.path;
-    setPostData((previous) => ({ ...previous, 'image': { file: event.target.files[0], filePath: URL.createObjectURL(event.target.files[0]), path: "", src: "" }}));
-  }
-
-  function removeImage(event) {
-    event.preventDefault();
-    deleteImageRef.current = postData.image.path;
-    setPostData((previous) => ({ ...previous, 'image': { file: null, filePath: "", path: "", src: "" }}));
-  }
 
   function handleFormChange(event) {
     const { name, value } = event.currentTarget;
@@ -81,16 +50,13 @@ export default function ProductCategoryModal({ editProductCategoryData, setEditP
     if (!checkEmptyFormFields()) return;
     setLoading(true);
     try {
-      const imageURL = await uploadFiles();
       const docRef = await addDoc(collection(db, "productCategories"), {
         ...postData,
-        'image': imageURL,
         'createdAt': serverTimestamp()
       })
       setProductCategories((previous) => [...previous,
       {
         ...postData,
-        'image': imageURL,
         'createdAt': Timestamp.fromDate(new Date()),
         'id': docRef.id
       }]);
@@ -106,11 +72,8 @@ export default function ProductCategoryModal({ editProductCategoryData, setEditP
     if (!checkEmptyFormFields()) return;
     setLoading(true);
     try {
-      if (deleteImageRef.current) deleteFile(deleteImageRef.current);
-      const imageURL = await uploadFiles();
       await setDoc(doc(db, "productCategories", editProductCategoryData.id), {
         ...postData,
-        'image': imageURL ? imageURL : { src: postData.image.src, path: postData.image.path },
         'createdAt': new Timestamp(editProductCategoryData.createdAt.seconds, editProductCategoryData.createdAt.nanoseconds)
       });
       setProductCategories((previous) => previous.map((product) => {
@@ -118,7 +81,6 @@ export default function ProductCategoryModal({ editProductCategoryData, setEditP
           return {
             ...postData,
             'id': editProductCategoryData.id,
-            'image': imageURL ? imageURL : { src: postData.image.src, path: postData.image.path },
             'createdAt': new Timestamp(editProductCategoryData.createdAt.seconds, editProductCategoryData.createdAt.nanoseconds)
           }
         } else {
@@ -152,21 +114,6 @@ export default function ProductCategoryModal({ editProductCategoryData, setEditP
               </div>
               <input name="name" onChange={handleFormChange} value={postData.name} type="text" className={"input input-bordered w-full" + (formErrors.name ? " border-red-600" : "")} />
             </label>
-            <div>
-              <input
-                className="mb-6"
-                type="file"
-                onChange={(event) => { if (event.target.files[0]) setImage(event) }}
-              />
-              <div className="flex flex-wrap">
-                {(postData.image.src || postData.image.filePath) &&
-                  <div className="h-36 max-h-36 mr-3 mb-3 flex">
-                    <img src={postData.image.src ? postData.image.src : postData.image.filePath} alt="Upload Preview" />
-                    <button className="btn h-full rounded-s-none" onClick={(event) => removeImage(event)}>X</button>
-                  </div>
-                }
-              </div>
-            </div>
             <div className="flex justify-end mt-10">
               <button className="btn mr-3">Close</button>
               {editProductCategoryData ?
