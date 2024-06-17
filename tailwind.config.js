fa3053335e09ae13ddc998f7a0ee088fa705317d
connor-kfitz <connorkfitzsimmons@gmail.com>
2024-06-17 00:47:44 -0400

    Adjusting Daisy UI Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require('daisyui')]
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ["light"],
+    base: false
+  },
 }
 
